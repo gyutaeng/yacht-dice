47bc736378d1ac9a381044c57de4fb33d2ec996c
KGB <Pandher0324@naver.com>
2026-09-14 22:06:22 +0900

    점수표 UI 뼈대 추가

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -2,16 +2,36 @@ extends Node2D
 
 const MAX_REROLLS := 2
 
+const CATEGORY_NAMES: Array[String] = [
+	"Aces",
+	"Deuces",
+	"Threes",
+	"Fours",
+	"Fives",
+	"Sixes",
+	"Choice",
+	"Four of a Kind",
+	"Full House",
+	"Small Straight",
+	"Large Straight",
+	"Yacht",
+]
+
 var dice_results: Array[int] = [1, 1, 1, 1, 1]
 var dice_locked: Array[bool] = [false, false, false, false, false]
 var rerolls_left: int = MAX_REROLLS
 
 var locked_style := StyleBoxFlat.new()
 
+var score_calculators: Array[Callable] = []
+var score_labels: Array[Label] = []
+var confirm_buttons: Array[Button] = []
+
 @onready var dice_labels: Array[Label] = [$Dice1, $Dice2, $Dice3, $Dice4, $Dice5]
 @onready var roll_button: Button = $RollButton
 @onready var new_turn_button: Button = $NewTurnButton
 @onready var reroll_label: Label = $RerollLabel
+@onready var score_list: VBoxContainer = $ScoreboardScroll/ScoreList
 
 
 func _ready() -> void:
@@ -30,7 +50,24 @@ func _ready() -> void:
 		label.mouse_filter = Control.MOUSE_FILTER_STOP
 		label.gui_input.connect(_on_dice_gui_input.bind(i))
 
+	score_calculators = [
+		_calc_aces,
+		_calc_deuces,
+		_calc_threes,
+		_calc_fours,
+		_calc_fives,
+		_calc_sixes,
+		_calc_choice,
+		_calc_four_of_a_kind,
+		_calc_full_house,
+		_calc_small_straight,
+		_calc_large_straight,
+		_calc_yacht,
+	]
+
+	_build_scoreboard()
 	_update_reroll_label()
+	_update_score_previews()
 
 
 func _on_dice_gui_input(event: InputEvent, index: int) -> void:
@@ -60,6 +97,7 @@ func _on_roll_button_pressed() -> void:
 
 	rerolls_left -= 1
 	_update_reroll_label()
+	_update_score_previews()
 	if rerolls_left <= 0:
 		roll_button.disabled = true
 
@@ -73,7 +111,92 @@ func _on_new_turn_button_pressed() -> void:
 		_update_dice_visual(i)
 
 	_update_reroll_label()
+	_update_score_previews()
 
 
 func _update_reroll_label() -> void:
 	reroll_label.text = "남은 리롤 횟수: %d" % rerolls_left
+
+
+func _build_scoreboard() -> void:
+	for i in CATEGORY_NAMES.size():
+		var row := HBoxContainer.new()
+		row.add_theme_constant_override("separation", 16)
+
+		var name_label := Label.new()
+		name_label.text = CATEGORY_NAMES[i]
+		name_label.custom_minimum_size = Vector2(200, 0)
+		row.add_child(name_label)
+
+		var score_label := Label.new()
+		score_label.text = "0"
+		score_label.custom_minimum_size = Vector2(60, 0)
+		score_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		row.add_child(score_label)
+		score_labels.append(score_label)
+
+		var confirm_button := Button.new()
+		confirm_button.text = "이 항목으로 확정"
+		confirm_button.pressed.connect(_on_confirm_pressed.bind(i))
+		row.add_child(confirm_button)
+		confirm_buttons.append(confirm_button)
+
+		score_list.add_child(row)
+
+
+func _on_confirm_pressed(index: int) -> void:
+	pass
+
+
+func _update_score_previews() -> void:
+	for i in CATEGORY_NAMES.size():
+		var value: int = score_calculators[i].call(dice_results)
+		score_labels[i].text = str(value)
+
+
+func _calc_aces(dice: Array[int]) -> int:
+	return 0
+
+
+func _calc_deuces(dice: Array[int]) -> int:
+	return 0
+
+
+func _calc_threes(dice: Array[int]) -> int:
+	return 0
+
+
+func _calc_fours(dice: Array[int]) -> int:
+	return 0
+
+
+func _calc_fives(dice: Array[int]) -> int:
+	return 0
+
+
+func _calc_sixes(dice: Array[int]) -> int:
+	return 0
+
+
+func _calc_choice(dice: Array[int]) -> int:
+	return 0
+
+
+func _calc_four_of_a_kind(dice: Array[int]) -> int:
+	return 0
+
+
+func _calc_full_house(dice: Array[int]) -> int:
+	return 0
+
+
+func _calc_small_straight(dice: Array[int]) -> int:
+	return 0
+
+
+func _calc_large_straight(dice: Array[int]) -> int:
+	return 0
+
+
+func _calc_yacht(dice: Array[int]) -> int:
+	return 0
